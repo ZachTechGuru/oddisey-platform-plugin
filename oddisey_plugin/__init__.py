9cfc8c2ac54ee0c85c6aed1e4004dfd427894e8f
Felipe Montoya <felipe.montoya@edunext.co>
2024-12-19 20:39:55 -0500

    feat: first commit

---
+++ oddisey_plugin/__init__.py
@@ -0,0 +1,5 @@
+"""
+Used for testing concepts from ZachTechGuru
+"""
+
+__version__ = '0.1.0'
---
+++ oddisey_plugin/apps.py
@@ -0,0 +1,13 @@
+"""
+oddisey_plugin Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class OddiseyPluginConfig(AppConfig):
+    """
+    Configuration for the oddisey_plugin Django application.
+    """
+
+    name = 'oddisey_plugin'
